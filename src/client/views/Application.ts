3ae7e248d3c6de9c8229bcaeb2294eead9891cac
vvanaparthi <venkat.rlu@gmail.com>
2018-05-02 14:41:20 -0600

    adding zoom in and zoom out to cube

--- src/client/views/Application.ts
+++ src/client/views/Application.ts
@@ -31,7 +31,16 @@ export class Application {
 
         // Log messages from the server
         connection.onmessage =  (e) => {
-            console.log(e.data);
+            let data = JSON.parse(e.data);
+            if (data.rawcode === 126) {
+                this.cube.position.z = this.cube.position.z + 1;
+               // this.mousewheel(1);
+            } else if (data.rawcode === 125) {
+               // this.mousewheel(-1);
+
+                this.cube.position.z = this.cube.position.z - 1;
+            }
+            // console.log(e.data);
         };
 
         this.scene = new THREE.Scene();
@@ -42,6 +51,30 @@ export class Application {
         appNode.appendChild( this.renderer.domElement );
 
     }
+
+    mousewheel( e ) {
+    let d = e;
+    d = ((d > 0) ? 1 :-1);
+
+    let cPos = this.camera.position;
+    if (isNaN(cPos.x) || isNaN(cPos.y) || isNaN(cPos.y))
+        return;
+
+    let r = cPos.x * cPos.x + cPos.y * cPos.y;
+    let sqr = Math.sqrt(r);
+    let sqrZ = Math.sqrt(cPos.z * cPos.z + r);
+
+    let nx = cPos.x + ((r === 0) ? 0 :(d * cPos.x / sqr));
+    let ny = cPos.y + ((r === 0) ? 0 :(d * cPos.y / sqr));
+    let nz = cPos.z + ((sqrZ === 0) ? 0 :(d * cPos.z / sqrZ));
+
+    if (isNaN(nx) || isNaN(ny) || isNaN(nz))
+        return;
+
+    cPos.x = nx;
+    cPos.y = ny;
+    cPos.z = nz;
+}
 
     initialize():void {
 
